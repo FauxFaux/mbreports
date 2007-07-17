059a429ca4f2e2323fd3b2607f99faca5e18b17f
Chris West (Faux) <git@goeswhere.com>
2007-07-17 19:44:25 +0000

    badurltypes.php was missing cover art that was from places other than amazon and archive.org.

--- badurltypes.php
+++ badurltypes.php
@@ -2,9 +2,19 @@
 require_once('database.inc.php');
 my_title();
 
-$res = pg_query("select url.description,album.name as name,album.id as release,url.url,url.id as urlid,lt_album_url.linkphrase from l_album_url join album on (album.id=l_album_url.link0) join url on (url.id=l_album_url.link1) join lt_album_url on (link_type=lt_album_url.id) where
-(link_type = 30 AND url NOT LIKE 'http://%amazon.%/%/%/%') OR (link_type = 34 AND url LIKE 'http://%amazon.%/%/%/%' AND url NOT LIKE 'http://%archive.org%/%/%/%') OR (link_type = 27 AND url NOT LIKE 'http://%imdb.%/%') OR
-(link_type = 25 AND url NOT LIKE 'http://%musicmoz.org/%/%/%') OR (link_type = 24 AND url NOT LIKE 'http://%discogs.%/%') OR (link_type = 23 AND url NOT LIKE 'http://%.wikipedia.org/%') ORDER BY link_type ASC,url ASC");
+$res = pg_query("select url.description,album.name as name,album.id as release,url.url,url.id as urlid,lt_album_url.linkphrase
+	from l_album_url join album on (album.id=l_album_url.link0)
+	join url on (url.id=l_album_url.link1)
+	join lt_album_url on (link_type=lt_album_url.id)
+	where
+	(link_type = 30 AND url NOT LIKE 'http://%amazon.%/%/%/%') OR
+	(link_type = 34 " /* cover art */. "AND (url NOT LIKE 'http://%archive.org/%' AND url NOT LIKE 'http://%cdbaby.%/%')) OR
+	(link_type = 27 AND url NOT LIKE 'http://%imdb.%/%') OR
+	(link_type = 25 AND url NOT LIKE 'http://%musicmoz.org/%/%/%') OR
+	(link_type = 24 AND url NOT LIKE 'http://%discogs.%/%') OR
+	(link_type = 23 AND url NOT LIKE 'http://%.wikipedia.org/%')
+	ORDER BY link_type ASC,url ASC
+");
 
 echo '<p>' . pg_num_rows($res) . ' found.</p>';
 
